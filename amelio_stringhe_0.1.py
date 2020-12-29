0e75c98f61cdebdd1fbde2dfcae147685215e1d3
claudia-amelio <74513728+claudia-amelio@users.noreply.github.com>
2020-12-29 19:49:40 +0100

    Add files via upload

---
+++ amelio_stringhe_0.1.py
@@ -0,0 +1,12 @@
+stringa = input(str("inserisci una stringa: "))
+
+print("stampo tutta la stringa:", stringa)
+
+for lettera in (stringa):
+        print(lettera)
+
+i=0
+for lettera in (stringa):
+    i=i+1
+
+print("il numero di lettere nella stringa è:", i)
