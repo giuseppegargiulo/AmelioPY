ac88d3d45932a3eaa5ac92bf3f5c5d52a7155ae6
claudietti <claudia.amelio.s13@liceoviconapoli.it>
2021-02-12 20:21:09 +0100

    Delete_files

--- test_matplotlib_fiocchetto.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-x = np.array([5,13,21,10,10,22,24,23,11,17,33,99])
-y = np.array([99,33,17,11,23,24,22,10,10,21,13,5])
-plt.plot(x,y)
-plt.show()
--- test_matplotlip_retta.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-x = np.array([5,10,15,20,25,30,35,40])
-y = np.array([3,6,9,12,15,18,21,24])
-plt.plot(x,y)
-plt.show()
