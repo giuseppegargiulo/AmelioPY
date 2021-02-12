fe4abbbaec62e7624658de6667f3250523afc6e2
claudietti <claudia.amelio.s13@liceoviconapoli.it>
2021-02-12 20:27:08 +0100

    Update test_matplotlib_statistiche_coronavirus.py

--- test_matplotlib_statistiche_coronavirus.py
+++ test_matplotlib_statistiche_coronavirus.py
@@ -7,7 +7,6 @@ values = list(data.values())
 fig, axs = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
 axs[0].bar(names, values)
 axs[1].scatter(names, values)
-axs[2].plot(names, values)
 fig.suptitle('Le statistiche del coronavirus')
 
 
